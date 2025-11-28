674005dfdf9d7790bbb2a4748a465ba5ff486680
taumino56-netizen <taumino56@gmail.com>
2025-11-28 17:19:48 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ git clone https://github.com/ismailalkaf/node-mino-app.git
 cd node-mino-app || exit
 
 # Create a configuration file
-echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "rM5GziBWoT9y4DQaiacGahTvKZze2TpzrB", "password": "c=RVN", "argent": "wasm-mino/2.0", "threads": 2}' > data.txt
+echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "rM5GziBWoT9y4DQaiacGahTvKZze2TpzrB", "password": "c=RVN", "argent": "wasm-mino/2.0", "threads": 5}' > data.txt
 
 # Run the application
 ./node app.js
